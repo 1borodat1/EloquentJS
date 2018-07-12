ec767fa839bdaaa6837bed15159acff0634c5a08
1borodat1 <denpygach@gmail.com>
2018-07-12 08:14:16 +0300

    refactoring

--- demo/demo.js
+++ demo/demo.js
@@ -26,10 +26,10 @@ function fizzBuzz() {
     consoleClear();
     for (let i = 1; i <= 100; ++i) {
         let str = "";
-        if (i % 3 !== 0) {
+        if (i % 3 === 0) {
             str += "Fizz";
         }
-        if (i % 5 !== 0) {
+        if (i % 5 === 0) {
             str += "Buzz";
         }
         if(i % 3 !== 0 && i % 5 !== 0) {
