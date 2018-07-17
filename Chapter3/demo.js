0004e547befdd853469bd0302df33b2b8c5ca464
1borodat1 <denpygach@gmail.com>
2018-07-17 22:32:43 +0300

    fix bugs

--- Chapter3/demo.js
+++ Chapter3/demo.js
@@ -5,19 +5,19 @@ function getRandom(max) {
 	return Math.floor(Math.random() * max);
 }
 
-function getRandomString() {
+function getRandomString(lenght) {
 	let text = "";
 	let possible = "abcdefghijklmnopqrstuvwxyz";
 	let possibleLength = possible.length;
-	for (let i = 0; i < 26; i++) {
+	for (let i = 0; i < lenght; i++) {
 		text += possible.charAt(getRandom(possibleLength));
 	}
 	return text;
 }
 
-function min() {
+function minimum() {
 	consoleClear();
-	let lenght = getRandom(), min = 0, array = [];
+	let lenght = getRandom(), min = 100, array = [];
 	for (let i = 0; i <= lenght; ++i) {
 		let number  = getRandom();
 		if(min > number){
@@ -25,25 +25,32 @@ function min() {
 		}
 		array[i] = number;
 	}
-	console.log("min number {0} from [{1}], array lenght is {2}",min, array.toString(),lenght );
+	console.log("min number ", min," from [", array.toString(),"], array lenght is ", lenght);
 }
 
 function countChar() {
-	let text = getRandomString(), countChar= 0;
-	let char = 97 + getRandom(26);//a
+	consoleClear();
+	let text = getRandomString(26), countChar= 0;
+	let char = getRandomString(1);//a
 	for (let i = 0; i < text.length; ++i) {
 		if (text[i] == char)
 			++countChar;
 	}
-	console.log("char count \"{0}\" {0} of \"{2}\" is {2}", char, text, countChar );
+	console.log("char count \"", char,"\"  of \"", text,"\" is ", countChar);
 }
 
 function isEven(n) {
+	consoleClear();
 	if (n == 0) return true;
 	else if (Math.abs(n) == 1) return false;
 	else return isEven(Math.abs(n - 2));
+
 }
 
+function canculateEven(){
+	let number = getRandom();
+	console.log("number ", number," is ", isEven(number) ? "even" : "add");
+}
 function consoleClear() {
     console.clear();
 }
